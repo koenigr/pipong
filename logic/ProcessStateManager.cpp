f875e2565cc3780d60a57ebbe86a27246dc29593
Koenig Regina <koenigr@tfpool15.informatik.uni-freiburg.de>
2019-06-29 16:33:08 +0200

    added GameLogic and ProcessStateManager

---
+++ logic/ProcessStateManager.cpp
@@ -0,0 +1,20 @@
+/*
+ * ProcessStateManager.cpp
+ *
+ *  Created on: 29.06.2019
+ *      Author: koenigr
+ */
+
+#include "ProcessStateManager.h"
+
+ProcessStateManager::ProcessStateManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ProcessStateManager::~ProcessStateManager() {
+	// TODO Auto-generated destructor stub
+}
+
+
+//TODO create state machine
