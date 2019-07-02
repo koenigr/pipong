dd357e8e754794d150861c95e69fe33b39839c83
KoenigR <koenig_regina@arcor.de>
2019-07-02 12:59:44 +0200

    lines are moving

--- test_code/lcd_test/LCDTest.cpp
+++ test_code/lcd_test/LCDTest.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "math.h"
 
 #include <chrono>
 
@@ -9,28 +10,77 @@
 
 using namespace GFX;
 
-void testCircle() {
-	OLEDDisplay disp(128, 128);
+OLEDDisplay disp(128, 128);
+int X_MIN = 0;
+int X_MAX = 125;
+int Y_MIN = 0;
+int Y_MAX = 125;
+int L_WIDTH = 50;
+
+void init() {
 
 	disp.setBgColor(COLOR_BLACK);
 	disp.setDrawColor(COLOR_WHITE);
 	disp.clearScreen();
 	disp.enable();
 
-	disp.fillCircle(60, 60, 10);
-
-	disp.drawLine(1, 20, 1, 100);
-	disp.drawLine(20, 1, 100, 1);
-	disp.drawLine(120, 20, 120, 100);
-	disp.drawLine(20, 120, 100, 120);
-
-
-	disp.flush();
-	std::this_thread::sleep_for(std::chrono::seconds(60));
 }
 
+void draw() {
+	disp.flush();
+//	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+}
+
+void ball() {
+
+        disp.fillCircle(60, 60, 10);
+}
+
+void line_p1(int pos) {
+
+        disp.drawLine(X_MIN, pos - L_WIDTH / 2, X_MIN, pos + L_WIDTH / 2);
+
+}
+
+void line_p2(int pos) {
+
+        disp.drawLine(pos - L_WIDTH, Y_MIN, pos + L_WIDTH, Y_MIN);
+
+}
+
+void line_p3(int pos) {
+
+        disp.drawLine(X_MAX, pos - L_WIDTH, X_MAX, pos + L_WIDTH);
+
+}
+
+void line_p4(int pos) {
+
+        disp.drawLine(pos - L_WIDTH, Y_MAX, pos + L_WIDTH, Y_MAX);
+
+}
+
+
 int main(void) {
+	init();
 	printf("Starting\n");
-	testCircle();
+
+	int i = 0;
+    while(true) {
+
+	int mv = sin(i)*20;
+
+	disp.clearScreen();
+	ball();
+	line_p1(60 + mv);
+	line_p2(60 + mv);
+	line_p3(60 + mv);
+	line_p4(60 + mv);
+	draw();
+	usleep(1000);
+	i += 1;
+    }
+
 	return 0;
 }
--- test_code/lcd_test/run.sh
+++ test_code/lcd_test/run.sh
@@ -3,5 +3,6 @@
 echo "Compiling LCDTest"
 g++ LCDTest.cpp Canvas.cpp Print.cpp OLEDDisplay.cpp -lmraa -pthread -std=c++11 -o LCDTest
 
+
 echo "Run LCDTest"
 ./LCDTest
