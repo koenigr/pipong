db271376bf26c07a91e2af447705d0b31129f349
Koenig Regina <koenigr@tfpool15.informatik.uni-freiburg.de>
2019-07-13 13:46:39 +0200

    added message_protocol

---
+++ message_protocol/Request.h
@@ -0,0 +1,15 @@
+#ifndef TEST_CODE_MESSAGE_PROTOCOL_REQUEST_H_
+#define TEST_CODE_MESSAGE_PROTOCOL_REQUEST_H_
+
+
+class Request {
+
+public:
+
+	Request();
+
+	Request(char out_str[], char ip_addr[], int port);
+};
+
+#endif /* TEST_CODE_MESSAGE_PROTOCOL_REQUEST_H_ */
+
---
+++ message_protocol/Response.h
@@ -0,0 +1,16 @@
+#ifndef TEST_CODE_MESSAGE_PROTOCOL_RESPONSE_H_
+#define TEST_CODE_MESSAGE_PROTOCOL_RESPONSE_H_
+
+
+class Response {
+
+public:
+
+	Response();
+
+	Response(char out_str[], char ip_addr[], int port);
+};
+
+#endif /* TEST_CODE_MESSAGE_PROTOCOL_RESPONSE_H_ */
+
+
