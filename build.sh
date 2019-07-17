e2841c1707e59665215ada9c919f7738a3034e79
KoenigR <koenig_regina@arcor.de>
2019-07-17 15:37:23 +0200

    adjusted build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+gamestate     message_protocol  pipong.cpp  test_code
+display   inputmanager  network           README.md   todo.txt
+docs      logic         Parameters.h      states      tools
+
+
 clear
 
 g++ pipong.cpp \
@@ -7,5 +13,9 @@ g++ pipong.cpp \
     tools/*.cpp \
     message_protocol/*.cpp \
     gamestate/*.cpp \
+    test_code/*.cpp \
+    display/*.cpp \
+    logic/*.cpp \
+    states/*.cpp \
     -o PiPong -std=c++11 -Wall -Wextra -Wpedantic 
 #-Werror
