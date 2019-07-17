e0451cb7c5c4a1c3b64635542e6b455dcec1edc6
KoenigR <koenig_regina@arcor.de>
2019-07-17 15:41:16 +0200

    removed test_code from build

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-gamestate     message_protocol  pipong.cpp  test_code
-display   inputmanager  network           README.md   todo.txt
-docs      logic         Parameters.h      states      tools
-
-
 clear
 
 g++ pipong.cpp \
@@ -13,7 +7,7 @@ g++ pipong.cpp \
     tools/*.cpp \
     message_protocol/*.cpp \
     gamestate/*.cpp \
-    test_code/*.cpp \
+#    test_code/*.cpp \
     display/*.cpp \
     logic/*.cpp \
     states/*.cpp \
