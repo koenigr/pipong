efccc02ca106eea755307a9da8498ed08b536db5
Koenig Regina <koenigr@tfpool23.informatik.uni-freiburg.de>
2019-07-18 21:56:58 +0200

    refactoring

--- pipong.cpp
+++ pipong.cpp
@@ -5,99 +5,17 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
-#include "Parameters.h"
-
 #include "network/UDPSocket.h"
-#include "tools/Tools.h"
 #include "network/AddressManager.h"
 #include "message_protocol/MessageProtocol.h"
 #include "gamestate/GameState.h"
-#include "states/State.h"
+#include "states/StateManager.h"
 
 AddressManager am;
 MessageProtocol mp;
 UDPSocket pi_socket;
 GameState gs;
-int player_self;
-State state;
-
-
-void init() {
-
-    printf("Initialize PiPong\n");
-
-    gs.init(player_self);
-
-    am.print_infos();
-    am.createOwnAddr();
-    am.createBroadcastAddr();
-
-    pi_socket.init(am.getOwnAddr());
-
-    printf("Initialization complete\n");
-}
-
-void receive_messages(sockaddr_in* recv) {
-
-	char buffer[BUFSIZE];
-
-    *recv = pi_socket.receiveMessage(buffer);
-
-}
-
-void find_peers() {
-
-    long int ms_start = Tools::getms();
-    long int ms_then = Tools::getms();
-
-    while(am.getNumOfParticipants() == 0) { // TODO loop until key press to proceed. Display all participants
-
-        // TODO: Wie erkennen, dass Versuch Verbindung und NUR dann hinzufügen?
-        sockaddr_in * recv;
-        receive_messages(recv);
-        char str[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(recv->sin_addr), str, INET_ADDRSTRLEN);
-
-        //if (strcmp(str, "255.127.0.0") != 0 && strcmp(str, "0.0.0.0") != 0 ) {
-            //am.addParticipant(recv);
-        //}
-
-        if ((ms_then - ms_start) > 1000) {
-
-            char request[REQUEST_SIZE];
-            mp.createRequest(request, player_self);
-            pi_socket.sendMessage(request, am.getBroadcastAddr());
-
-            ms_start = Tools::getms();
-
-        }
-        ms_then = Tools::getms();
-
-    }
-
-    char message[] = "New Participant ";
-    Tools::print_address(am.getParticipant(0), message);
-
-
-}
-
-void process_input() {
-
-}
-
-void update_game_state() {
-
-}
-
-void deploy_game_state() {
-	char message[] = "Gamestate changed";
-	pi_socket.sendMessage(message, am.getBroadcastAddr());
-}
-
-void display() {
-
-}
-
+StateManager state_manager;
 
 int main(int argc, char* argv[]) {
 
@@ -106,36 +24,15 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    player_self = atoi(argv[1]);
+    int player_self = atoi(argv[1]);
 
-	init();
+    state_manager.init(player_self, gs, am, pi_socket);
 
-	find_peers();
+    state_manager.findPeers(am, pi_socket, mp, gs);
 
-//    long int ms_start = Tools::getms();
-//    long int ms_then = Tools::getms();
+    state_manager.mainLoop(player_self, am, mp, pi_socket, gs);
 
-//    while(true) {
-
-//    	//receive_messages();
-
-//    	process_input();
-
-//    	update_game_state();
-
-//    	if ((ms_then - ms_start) > 1000) {
-
-//    		//deploy_game_state();
-//    		ms_start = Tools::getms();
-
-//    	}
-//    	ms_then = Tools::getms();
-
-//    	display();
-
-//    }
-
-    state.showPoints();
+    state_manager.showPoints();
 
     return 0;
 }
