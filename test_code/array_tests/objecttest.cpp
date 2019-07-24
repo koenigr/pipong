ebae0e8f32ccea5a286f2f0755f7021a69d102dc
Koenig Regina <koenigr@tfpool28.informatik.uni-freiburg.de>
2019-07-24 16:56:58 +0200

    Example for sstream in objecttest.cpp

--- test_code/array_tests/objecttest.cpp
+++ test_code/array_tests/objecttest.cpp
@@ -1,5 +1,11 @@
 #include "testobject.h"
 #include <iostream>
+#include <sstream>
+#include <stdio.h>
+
+#define HEADER "abc:"
+#define INT " %d"
+#define COMMA " ,"
 
 int main() {
     TestObject o1;
@@ -12,5 +18,20 @@ int main() {
 
     std::cout << "object1 after: " << o1.getI() << std::endl;
     std::cout << "object2 after: " << o2.getI() << std::endl;
+
+
+    std::stringstream x;
+    x << "abc:  ASD " << 5 << "," << "6,7,8";
+    std::string s = x.str();
+    std::cout << s << std::endl;
+
+    int y, z;
+    char mt[5];
+    int r = sscanf(s.c_str(), HEADER " %4s" INT COMMA INT, mt, &y, &z);
+    std::cout << r << std::endl;
+    std::cout << mt << std::endl;
+    //if (r >= 2) {
+        std::cout << y << " " << z << std::endl;
+    //}
 
 }
