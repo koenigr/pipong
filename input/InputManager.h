afd07b8b810d8389f903d79fff30e760de093722
koenigr <koenig_regina@arcor.de>
2019-07-29 23:44:57 +0200

    nn

--- input/InputManager.h
+++ input/InputManager.h
@@ -10,7 +10,7 @@ private:
     static mraa_gpio_context bt_ct;
     static int getPlayerPos(double accel);
     static void getAccel(mraa_i2c_context i2c, double *data);
-    static int decodeS16BE(uint8_t *buf);
+    static int16_t decodeS16BE(uint8_t *buf);
 public:
     static void gpio_dir(mraa_gpio_context g, mraa_gpio_dir_t dir);
     static _gpio* gpio_init(int pin, mraa_gpio_dir_t dir);
