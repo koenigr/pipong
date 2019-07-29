25a5a3495b7399297467bd6d9e1042f64d8022e4
koenigr <koenig_regina@arcor.de>
2019-07-29 22:06:22 +0200

    ff

--- display/Display.cpp
+++ display/Display.cpp
@@ -61,10 +61,8 @@ void Display::drawPlayer(const GameState gs) {
     std::cout << "TODO: Player rotate into wrong direction\n";
     for (int i = 0; i < 4; i++) {
         Player player = gs.getPlayer(i);
+        std::out << player.toString() << std::endl;
         disp.drawLine(player.getLeftEndX(), player.getLeftEndY(), player.getRightEndX(), player.getRightEndY());
-//        std::cout << "Player " << i
-//                << ": (" << player.getLeftEndX() << "," << player.getLeftEndY() << "),("
-//                << player.getRightEndX() << "," << player.getRightEndY() << ")\n";
     }
 }
 
