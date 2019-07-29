3722f7b54a5bd5f910fd45fe513b24a84ac84b4d
koenigr <koenig_regina@arcor.de>
2019-07-29 22:30:07 +0200

    added player position if player not active

--- gamestate/Player.cpp
+++ gamestate/Player.cpp
@@ -79,21 +79,25 @@ std::string Player::toString() const {
 }
 
 int Player::getLeftEndX() const { // TODO wrong order
+    if (!isActive()) return DISPLAY_SIZE * x_axis * oth_axis;
     return (DISPLAY_SIZE/2 - position - PADDLE_WIDTH /2)*(!x_axis)
             +DISPLAY_SIZE * x_axis * oth_axis;
 }
 
 int Player::getRightEndX() const {
+    if (!isActive()) return DISPLAY_SIZE * !x_axis * oth_axis ;
     return (DISPLAY_SIZE/2 - position + PADDLE_WIDTH /2)*(!x_axis)
             +DISPLAY_SIZE * x_axis * oth_axis;
 }
 
 int Player::getLeftEndY() const {
+    if (!isActive()) return DISPLAY_SIZE * !(x_axis * !oth_axis);
     return (DISPLAY_SIZE/2 - position - PADDLE_WIDTH /2)*(x_axis)
             +DISPLAY_SIZE * !x_axis * oth_axis;
 }
 
 int Player::getRightEndY() const {
+    if (!isActive()) return DISPLAY_SIZE * !(!x_axis*!oth_axis);
     return (DISPLAY_SIZE/2 - position + PADDLE_WIDTH /2)*(x_axis)
             +DISPLAY_SIZE * !x_axis * oth_axis;
 }
--- test_code/player_test/playertest.cpp
+++ test_code/player_test/playertest.cpp
@@ -8,36 +8,36 @@ int main() {
     GameState gs0;
     gs0.init(0);
     //std::cout << gs0.toString() << std::endl;
-    gs0.setPlayerActive(1,1);
-    gs0.setPlayerActive(1,2);
-    gs0.setPlayerActive(1,3);
+//    gs0.setPlayerActive(1,1);
+//    gs0.setPlayerActive(1,2);
+//    gs0.setPlayerActive(1,3);
     std::cout << gs0.toString() << std::endl;
     std::cout << gs0.getSelf().toString() << std::endl;
 
     GameState gs1;
     gs1.init(1);
     //std::cout << gs1.toString() << std::endl;
-    gs1.setPlayerActive(1,0);
-    gs1.setPlayerActive(1,2);
-    gs1.setPlayerActive(1,3);
+//    gs1.setPlayerActive(1,0);
+//    gs1.setPlayerActive(1,2);
+//    gs1.setPlayerActive(1,3);
     std::cout << gs1.toString() << std::endl;
     std::cout << gs1.getSelf().toString() << std::endl;
 
     GameState gs2;
     gs2.init(2);
     //std::cout << gs2.toString() << std::endl;
-    gs2.setPlayerActive(1,0);
-    gs2.setPlayerActive(1,1);
-    gs2.setPlayerActive(1,3);
+//    gs2.setPlayerActive(1,0);
+//    gs2.setPlayerActive(1,1);
+//    gs2.setPlayerActive(1,3);
     std::cout << gs2.toString() << std::endl;
     std::cout << gs2.getSelf().toString() << std::endl;
 
     GameState gs3;
     gs3.init(3);
     //std::cout << gs3.toString() << std::endl;
-    gs3.setPlayerActive(1,0);
-    gs3.setPlayerActive(1,1);
-    gs3.setPlayerActive(1,2);
+//    gs3.setPlayerActive(1,0);
+//    gs3.setPlayerActive(1,1);
+//    gs3.setPlayerActive(1,2);
     std::cout << gs3.toString() << std::endl;
     std::cout << gs3.getSelf().toString() << std::endl;
 
