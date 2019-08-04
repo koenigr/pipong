d9552c058f702c46f8ad141eebbaf7e8ec6b8c03
koenigr <koenig_regina@arcor.de>
2019-08-04 15:06:39 +0200

    added gameLoop

--- message_protocol/MessageProtocol.cpp
+++ message_protocol/MessageProtocol.cpp
@@ -111,10 +111,6 @@ std::string MessageProtocol::createFinish(GameState gs) {
 
 void MessageProtocol::evalMessage(std::string message, GameState& gs) {
 
-    std::cout << "MessageProtocol::evalMessage() start...\n";
-
-    std::cout << message << std::endl;
-
     char tp[4];
     char rm[BUFSIZE];
     int r = sscanf(message.c_str(), MAIN_HEADER DELIMITER "%3s" DELIMITER REMAIN, tp, rm);
